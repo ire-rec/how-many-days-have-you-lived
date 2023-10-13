e57770f940860649db7884ddc476a66965b9d39b
ire-rec <i.recavarren@gmail.com>
2023-10-13 14:23:27 +0200

    add global var

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use script'
+
+// GLOBAL VAR
+const age = document.querySelector('.js_age');
+const discoverBtn = document.querySelector('.js_discover');
+const messageUser = document.querySelector('.js_message');
+const birthdate = document.querySelector('.js_birthDate');
