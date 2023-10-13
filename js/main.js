09dcdc76ece205e8428711e83d3681badf2ca641
ire-rec <i.recavarren@gmail.com>
2023-10-13 18:47:27 +0200

    add new Date and getTime

--- js/main.js
+++ js/main.js
@@ -5,3 +5,8 @@ const age = document.querySelector('.js_age');
 const discoverBtn = document.querySelector('.js_discover');
 const messageUser = document.querySelector('.js_message');
 const birthdate = document.querySelector('.js_birthDate');
+
+//------------- FUNCTIONS------------------//
+const today = new Date();
+console.log(today);
+const todayInTime = (today.getTime()); //milisegundos desde 1-1-70 hasta hoy
