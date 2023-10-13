eb365a7fb52d48cb8c6296149b9ba496bf7beae8
ire-rec <i.recavarren@gmail.com>
2023-10-13 19:07:45 +0200

    add button and answer

--- js/main.js
+++ js/main.js
@@ -9,4 +9,12 @@ const birthdate = document.querySelector('.js_birthDate');
 //------------- FUNCTIONS------------------//
 const today = new Date();
 console.log(today);
-const todayInTime = (today.getTime()); //milisegundos desde 1-1-70 hasta hoy
+const todayInTime = (today.getTime()); //milliseconds from 1-1-70 until today
+
+//BUTTON
+discoverBtn.addEventListener('click',(event)=>{
+    event.preventDefault();
+    const birthdateInTime = birthdate.valueAsNumber;//milliseconds 
+    messageUser.innerHTML= (`hi, you have lived for ${(todayInTime - birthdateInTime)/1000} milliseconds`); //milliseconds from 1-1-70 until input date
+    });
+    // today minus input date
