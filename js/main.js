6dba65444b0bd777a74cb350eefe7e645259451f
ire-rec <i.recavarren@gmail.com>
2023-12-28 17:49:49 +0100

    add reset function

--- js/main.js
+++ js/main.js
@@ -1,30 +1,39 @@
-'use script'
+"use script";
 
 // GLOBAL VAR
-const discoverBtn = document.querySelector('.js_discover');
-const messageUser = document.querySelector('.js_message');
-const dateOfBirth = document.querySelector('.js_dateOfBirth');
-
+const discoverBtn = document.querySelector(".js_discover");
+const messageUser = document.querySelector(".js_message");
+const dateOfBirth = document.querySelector(".js_dateOfBirth");
+const form = document.querySelector(".js_form");
+const resetBtn = document.querySelector(".js_reset");
 
 //------------- FUNCTIONS------------------//
 const today = new Date();
-const todayInTime = (today.getTime()); //milliseconds from 1-1-70 until today
+const todayInTime = today.getTime(); //milliseconds from 1-1-70 until today
 
-//BUTTON
-discoverBtn.addEventListener('click',(event)=>{
-    console.log(event.currentTarget);
-    event.preventDefault();
-    const birthdateInTime = dateOfBirth.valueAsNumber;//milliseconds 
-    const hoursYouLived = (((todayInTime - birthdateInTime)/1000)/3600);
-    const integerHours = Math.trunc(hoursYouLived);
+//BUTTONS
+discoverBtn.addEventListener("click", (event) => {
+  console.log(event.currentTarget);
+  event.preventDefault();
+  const birthdateInTime = dateOfBirth.valueAsNumber; //milliseconds
+  const hoursYouLived = (todayInTime - birthdateInTime) / 1000 / 3600;
+  const integerHours = Math.trunc(hoursYouLived);
 
-    if (integerHours) {
-        messageUser.innerHTML = (`hi, you have lived for ${integerHours} hours`)}
-    else {
-        messageUser.innerHTML = ('You must indicate your date of birth')} //hours from 1-1-70 until input date 
+  if (integerHours) {
+    messageUser.innerHTML = `hi, you have lived for ${integerHours} hours`;
+  } else {
+    messageUser.innerHTML = "You must indicate your date of birth";
+  } //hours from 1-1-70 until input date
 });
-       
 
 // total formula = today - input date
- 
-   
+
+function reset() {
+  form.reset();
+  messageUser.innerHTML = "";
+}
+
+resetBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  reset();
+});
