f403b41cd673cc94b4a8221750b4e6eb1e96c2ad
ire-rec <i.recavarren@gmail.com>
2024-01-18 23:41:30 +0100

    add readme

--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,5 @@
-# how-many-days-have-you-lived
-Practice exercises - new Date - JS
+# Discover how many days have you lived
+
+A través de esta aplicación el usuario puede descubrir cuántos días ha vivido. Construida con JavaScript, el funcionamiento es muy sencillo: el usuario solo debe indicar su fecha de nacimiento en el input correspondiente y clicar "Find out!"
+
+He trabajado con el objeto Date y con su método getTime();. A partir de ahí solo he realizado operaciones matemáticas básicas para medir el tiempo en días.
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,30 +1,31 @@
 "use strict";
 
-// GLOBAL VAR
+// Global var
 const discoverBtn = document.querySelector(".js_discover");
 const messageUser = document.querySelector(".js_message");
 const dateOfBirth = document.querySelector(".js_dateOfBirth");
 const form = document.querySelector(".js_form");
 const resetBtn = document.querySelector(".js_reset");
 
-//------------- FUNCTIONS------------------//
+// Special var
 const today = new Date();
 const todayInTime = today.getTime(); //milliseconds from 1-1-70 until today
 
+//Functions
 const numbers = () => {
-  const birthdateInTime = dateOfBirth.valueAsNumber; //milliseconds
+  const birthdateInTime = dateOfBirth.valueAsNumber;
   const hoursYouLived = (todayInTime - birthdateInTime) / 1000 / 3600 / 24;
-  const integerHours = Math.trunc(hoursYouLived); //Math.trunc return número entero
+  const integerHours = Math.trunc(hoursYouLived);
   const numberFormat = new Intl.NumberFormat("es-ES");
   const totalHours = numberFormat.format(integerHours);
+
   if (integerHours) {
     messageUser.innerHTML = `hi, you have lived for ${totalHours} days`;
     messageUser.classList.add("msg_span");
   } else {
     messageUser.innerHTML = "You must indicate your date of birth";
     messageUser.classList.add("msg_span");
-  } //hours from 1-1-70 until input date
-  // total formula = today - input date
+  }
 };
 
 const reset = () => {
@@ -43,3 +44,5 @@ resetBtn.addEventListener("click", (event) => {
   event.preventDefault();
   reset();
 });
+
+// total formula = today - input date //
